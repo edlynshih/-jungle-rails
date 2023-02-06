4a4484f66049f12a64d84645fd5631e64c8bdb3b
Edlyn Shih <edlyn0418@gmail.com>
2023-02-06 15:28:44 -0500

    Update application controller to look up the user and save their user obj if they're logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,4 +29,19 @@ class ApplicationController < ActionController::Base
     }
     cookies[:cart]
   end
+
+  # Prevent CSRF attacks by raising an exception.
+  protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  # allows us to use current_user in view files
+  helper_method :current_user
+
+  #sends user to login page if they aren't logged in
+  def authorize
+    redirect_to '/login' unless current_user
+  end
+
 end
