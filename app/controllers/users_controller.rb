f1a3fe338d3ae49725f471ceccf93eb57f41a8bb
Edlyn Shih <edlyn0418@gmail.com>
2023-02-06 20:03:30 -0500

    Fix bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
+    @user = User.new(user_params)
+    if @user.save
       session[:user_id] = user.id
       redirect_to '/'
     else
@@ -17,7 +17,8 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(
-      :name, 
+      :first_name,
+      :last_name,
       :email, 
       :password, 
       :password_confirmation
